d9b912fed32d881e4e7b97d67044a6a16ebea965
tarafdar <naif.tarafdar@mail.utoronto.ca>
2019-09-09 14:05:12 -0400

    made deployment more general

--- docker/deploy_container.sh
+++ docker/deploy_container.sh
@@ -1,9 +1,14 @@
+echo "Please enter vivado install path"
+read vivado_dir
+echo "Please enter open port"
+read port
 
-
-docker run -ti -p 9000:9000 \
-           -d -v /opt/Xilinx:/opt/Xilinx \
+docker run -ti -p $port:9000 \
+           -d -v $vivado_dir:/opt/Xilinx \
            --rm \
            -e DISPLAY=$DISPLAY \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
            galapagos_docker
            bash
+
+
