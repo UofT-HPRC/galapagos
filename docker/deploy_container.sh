acfd88e4f847e0e7e62790de7b036ced6bfb359d
tarafdar <naif.tarafdar@mail.utoronto.ca>
2019-09-10 00:05:19 -0400

    full net in container

--- docker/deploy_container.sh
+++ docker/deploy_container.sh
@@ -1,10 +1,9 @@
 echo "Please enter vivado install path"
 read vivado_dir
-echo "Please enter open port"
-read port
 
-docker run -ti -p $port:9000 \
-           -d -v $vivado_dir:/opt/Xilinx \
+docker run -ti \
+	   --net=host \
+	   -d -v $vivado_dir:/opt/Xilinx \
            --rm \
            -e DISPLAY=$DISPLAY \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
