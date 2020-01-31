d8982c48bd2024fd7eb59d134a62b88ebd429b38
Marco Merlini <mahkoe@gmail.com>
2020-01-31 13:46:18 -0500

    Fixed a few minor errors in IP packaging scripts

--- middleware/hls/axistreamstats/ip_maker.tcl
+++ middleware/hls/axistreamstats/ip_maker.tcl
@@ -20,17 +20,30 @@ set part_name [lindex $argv 2]
 set project_name ${ip_name}_tmp_proj
 create_project ${project_name} ${project_name} -part ${part_name}
 add_files ${out_dir}/src
-ipx::package_project -root_dir ${out_dir} -vendor Marco_Merlini -library fpga_bpf -taxonomy /UserIP
+ipx::package_project -root_dir ${out_dir} -vendor mmerlini.ca -library galapagos -taxonomy /UserIP
 
-###PUT YOUR COMMANDS HERE
+set_property tooltip {Width of AXI Stream channel} [ipgui::get_guiparamspec -name "DATA_WIDTH" -component [ipx::current_core] ]
+set_property widget {textEdit} [ipgui::get_guiparamspec -name "DATA_WIDTH" -component [ipx::current_core] ]
+
+set_property display_name {Counter Width} [ipgui::get_guiparamspec -name "CNT_WIDTH" -component [ipx::current_core] ]
+set_property tooltip {Number of bits in packet/flit count} [ipgui::get_guiparamspec -name "CNT_WIDTH" -component [ipx::current_core] ]
+set_property widget {textEdit} [ipgui::get_guiparamspec -name "CNT_WIDTH" -component [ipx::current_core] ]
+
+set_property widget {comboBox} [ipgui::get_guiparamspec -name "RESET_TYPE" -component [ipx::current_core] ]
+set_property value_validation_type pairs [ipx::get_user_parameters RESET_TYPE -of_objects [ipx::current_core]]
+set_property value_validation_pairs {{No reset} 0 {Active high} 1 {Active low} 2} [ipx::get_user_parameters RESET_TYPE -of_objects [ipx::current_core]]
+
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.RESET_TYPE')) = 1} [ipx::get_bus_interfaces rst -of_objects [ipx::current_core]]
+
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.RESET_TYPE')) = 2} [ipx::get_bus_interfaces rstn -of_objects [ipx::current_core]]
 
 # Uncomment these lines after copying TCL commands from gui
 
-###ipx::create_xgui_files [ipx::current_core]
-###ipx::update_checksums [ipx::current_core]
-###ipx::save_core [ipx::current_core]
-###close_project 
-###exit
+ipx::create_xgui_files [ipx::current_core]
+ipx::update_checksums [ipx::current_core]
+ipx::save_core [ipx::current_core]
+close_project 
+exit
 
 
 
