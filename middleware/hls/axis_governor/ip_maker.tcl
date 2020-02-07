fbbd2a074ca2c4fbfca424e4b9018ced36f99de6
Marco Merlini <mahkoe@gmail.com>
2020-02-07 17:24:06 -0500

    Tidied up AXIS Governor IP packager script

--- middleware/hls/axis_governor/ip_maker.tcl
+++ middleware/hls/axis_governor/ip_maker.tcl
@@ -1,19 +1,6 @@
 # Call as:
 # vivado -mode tcl -nolog -nojournal -source scripts/ip_package.tcl -tclargs $out_dir $ip_name $part_name
 
-
-# INSTRUCTIONS
-# ------------
-# The first time you run this tcl, it will open the Vivado GUI in the IP 
-# packaging mode. Go ahead and edit your IP the way you normally would, taking 
-# care to copy and paste all the TCL commands that Vivado generates into the
-# location labeled below.
-# When you're finished, remove the "start_gui" line and uncomment all the lines
-# at the end of this script
-
-
-start_gui; # Remove this line after copying TCL commands (see below)
-
 set out_dir [lindex $argv 0]
 set ip_name [lindex $argv 1]
 set part_name [lindex $argv 2]
@@ -22,15 +9,22 @@ create_project ${project_name} ${project_name} -part ${part_name}
 add_files ${out_dir}/src
 ipx::package_project -root_dir ${out_dir} -vendor Marco_Merlini -library fpga_bpf -taxonomy /UserIP
 
-###PUT YOUR COMMANDS HERE
+# Create dropdown box for reset type
+set_property widget {comboBox} [ipgui::get_guiparamspec -name "RESET_TYPE" -component [ipx::current_core] ]
+set_property value_validation_type pairs [ipx::get_user_parameters RESET_TYPE -of_objects [ipx::current_core]]
+set_property value_validation_pairs {{No reset} 0 {Active high} 1 {Active low} 2} [ipx::get_user_parameters RESET_TYPE -of_objects [ipx::current_core]]
 
-# Uncomment these lines after copying TCL commands from gui
+# Auto-hide rst port when unused
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.RESET_TYPE')) = 1} [ipx::get_bus_interfaces rst -of_objects [ipx::current_core]]
 
-###ipx::create_xgui_files [ipx::current_core]
-###ipx::update_checksums [ipx::current_core]
-###ipx::save_core [ipx::current_core]
-###close_project 
-###exit
+# Auto-hide rstn port when unused
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.RESET_TYPE')) = 2} [ipx::get_bus_interfaces rstn -of_objects [ipx::current_core]]
+
+ipx::create_xgui_files [ipx::current_core]
+ipx::update_checksums [ipx::current_core]
+ipx::save_core [ipx::current_core]
+close_project 
+exit
 
 
 
