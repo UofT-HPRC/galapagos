3398fe367987a8922419a80fa1f28e518f4ee00f
smilepengg <smile.peng@mail.utoronto.ca>
2020-07-14 17:29:46 -0400

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,13 +6,6 @@ cd Sender
 source get_receiver_id.sh
 source get_receiver_ip.sh
 
-source create_sender.sh
 #deploying sender container
+source create_sender.sh
 source deploy_sender.sh
-
-#getting id pf sender container to cp ip file to it
-#source get_id.sh
-#export SENDER_ID=$(cat id.txt)
-
-#cp ip file of receiver
-#docker cp ip.txt $SENDER_ID:/home/user  
