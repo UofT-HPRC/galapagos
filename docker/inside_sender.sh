1273de95a8885f4ba43c76352349fef32a36b7d7
smilepengg <smile.peng@mail.utoronto.ca>
2020-08-12 00:21:38 +0000

    update inside_sender.sh to initialize vivado

--- docker/inside_sender.sh
+++ docker/inside_sender.sh
@@ -1,6 +1,10 @@
+cd /opt/Xilinx
+source vivado_setup "2018.1"
+
 cd /home/user/galapagos
 sudo chmod -R 777 hlsBuild
 sudo chmod -R 777 projects
+
 cd middleware/python
 source 03_building_the_project.py
 
