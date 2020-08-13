297dc0b2eaab86f65b0609ae066e7c603a5f2f33
smilepengg <smile.peng@mail.utoronto.ca>
2020-08-13 14:37:52 -0400

    Update 01_setup_and_creating_ip_cores.py

--- middleware/python/01_setup_and_creating_ip_cores.py
+++ middleware/python/01_setup_and_creating_ip_cores.py
@@ -23,7 +23,7 @@ vivado_version=2018.1
 hls_version=2018.1
 galapagos_board=adm-8k5-debug
 vivado_path=/opt/Xilinx/Vivado
-hls_path=/opt/Xilinx/Vivado_HLS
+hls_path=/opt/Xilinx/$hls_version
 
 source init.sh $galapagos_path $vivado_path $hls_path $vivado_version $hls_version
 galapagos-update-board $galapagos_board
