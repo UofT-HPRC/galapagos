1382f7c41529885b950721084f257fd4113796b9
tarafdar <tarafdar12@gmail.com>
2021-08-10 00:28:01 +0000

    updated hls router

--- middleware/hls/router/src/router.cpp
+++ middleware/hls/router/src/router.cpp
@@ -18,9 +18,10 @@ void router(
         const ap_uint<NETWORK_HEADER_LENGTH> network_table[256],
         const ap_uint <NETWORK_HEADER_LENGTH> network_addr,
         galapagos_interface & stream_in,
-		galapagos_interface  & stream_out_switch,
-		galapagos_interface  & stream_out_network,
-        ap_uint<NETWORK_HEADER_LENGTH> * addr_out
+	galapagos_interface  & stream_out_switch,
+	galapagos_interface  & stream_out_network,
+        volatile ap_uint<NETWORK_HEADER_LENGTH> * addr_out,
+        volatile int * state_out
 
 ){
 #pragma HLS INTERFACE ap_ctrl_none port=return
@@ -34,33 +35,75 @@ void router(
 	galapagos_packet packetIn;
 	galapagos_packet packetOut;
 
-	bool inFPGA = false;
-    ap_uint <NETWORK_HEADER_LENGTH> network_addr_in;
-
-	if(!stream_in.empty()){
-		packetIn = stream_in.read();
-
-        packetOut.data = packetIn.data;
-        packetOut.last = packetIn.last;
-        packetOut.keep = packetIn.keep;
-        packetOut.dest = packetIn.dest;
-        packetOut.id = packetIn.id;
-#ifdef PACKET_USER_LENGTH
-#if PACKET_USER_LENGTH > 0
-        packetOut.user = packetIn.user;
-#endif
-#endif
-        network_addr_in = network_table[packetIn.dest];
-        *addr_out = network_addr_in;
-        inFPGA = (network_addr == network_addr_in);
+	//bool inFPGA = false;
+	
+        static enum sState {TX_IDLE = 0, TX_WRITE_SWITCH, TX_WRITE_NETWORK} sinkState;
         
-        if(inFPGA){
-        	stream_out_switch.write(packetOut);
+        switch(sinkState){
+            case TX_IDLE: 
+                if(!stream_in.empty()){
+                    packetIn = stream_in.read();
+                    int index = packetIn.dest;
+                    ap_uint <NETWORK_HEADER_LENGTH> network_addr_in = network_table[index * 4];
+                    *addr_out = network_addr_in;
+                    if(network_addr_in == network_addr){
+                        sinkState = TX_WRITE_SWITCH;
+                        stream_out_switch.write(packetIn); 
+                    }
+                    else{
+                        sinkState = TX_WRITE_NETWORK;
+                        stream_out_network.write(packetIn); 
+                    }
+                }
+                break;
+            case TX_WRITE_SWITCH:
+                if(!stream_in.empty()){
+                    packetIn = stream_in.read();
+                    if(packetIn.last)
+                        sinkState = TX_IDLE;
+                    stream_out_switch.write(packetIn);
+                }
+                break;
+            case TX_WRITE_NETWORK:
+                if(!stream_in.empty()){
+                    packetIn = stream_in.read();
+                    if(packetIn.last)
+                        sinkState = TX_IDLE;
+                    stream_out_network.write(packetIn);
+                }
+                break;
+            default:
+                sinkState = TX_IDLE;
+                break;
         }
-        else{
-        	stream_out_network.write(packetOut);
-        }
-	}
+
+        *state_out = (int)sinkState;
+
+
+//	if(!stream_in.empty()){
+//		packetIn = stream_in.read();
+//
+//                packetOut.data = packetIn.data;
+//                packetOut.last = packetIn.last;
+//                packetOut.keep = packetIn.keep;
+//                packetOut.dest = packetIn.dest;
+//                packetOut.id = packetIn.id;
+//#ifdef PACKET_USER_LENGTH
+//#if PACKET_USER_LENGTH > 0
+//                packetOut.user = packetIn.user;
+//#endif
+//#endif
+//        network_addr_in = network_table[packetIn.dest];
+//        *addr_out = network_addr_in;
+//        inFPGA = (network_addr == network_addr_in);
+//        
+//        if(inFPGA){
+//        	stream_out_switch.write(packetOut);
+//        }
+//        else{
+//        	stream_out_network.write(packetOut);
+//        }
+//	}
 
 
 }
