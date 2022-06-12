4d960180f32359b8ec7a839b18a11c4963443dcc
yugao-uoft <yujohn.gao@mail.utoronto.ca>
2022-06-11 21:28:59 -0400

    delete backup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ parser.add_argument("-vd", "--viv_dir", help="absolute path of vivado", default=
 parser.add_argument("-hd", "--hls_dir", help="absolute path of vivado_hls", default='')
 parser.add_argument("-vv", "--viv_ver", help="version of vivado", default='')
 parser.add_argument("-hv", "--hls_ver", help="version of vivado_hls", default='')
-
+parser.add_argument("-pn", "--par_name", help="set part name", default='')
+parser.add_argument("-bn", "--boa_name", help="set board name", default='')
 
 args = parser.parse_args()
 
@@ -83,6 +84,17 @@ hls_ver_q = {
           'filter': lambda val: val.lower()
           }
 
+par_name_q = {
+          'type': 'input',
+          'name': 'par_name',
+          'message': 'What is the part name you want to use? (default=xczu19eg-ffvc1760-2-i)'
+          }
+
+boa_name_q = {
+          'type': 'input',
+          'name': 'boa_name',
+          'message': 'What is the board name you want to use? (default=sidewinder)'
+          }
 
 
 if(args.dir ==''):
@@ -115,6 +127,19 @@ if(args.viv_ver == ''):
 else:
     hls_ver = args.hls_ver
 
+
+if(args.par_name == ''):
+    questions.append(par_name_q)
+    par_name = ''
+else:
+    par_name = args.par_name
+
+if(args.boa_name == ''):
+    questions.append(boa_name_q)
+    boa_name = ''
+else:
+    boa_name = args.boa_name
+
 answers = prompt(questions, style=style)
 
 if answers['galapagos_dir'] == '':
@@ -133,17 +158,28 @@ if answers['vivado_ver'] == '':
 if answers['hls_ver'] == '':
     answers['hls_ver'] = '2018.1'
 
+if answers['par_name'] == '':
+    answers['par_name'] = 'xczu19eg-ffvc1760-2-i'
+
+if answers['boa_name'] == '':
+    answers['boa_name'] = 'sidewinder'
+
 
 print('ENVIRONMENT VARIABLES SET:')
 env_var = {'GALAPAGOS_PATH': answers['galapagos_dir'],
            'GALAPAGOS_VIVADO_PATH' : answers['vivado_dir'],
            'GALAPAGOS_HLS_PATH' : answers['hls_dir'],
            'GALAPAGOS_VIVADO_VERSION' : answers['vivado_ver'],
-           'GALAPAGOS_HLS_VERSION' : answers['hls_ver']
+           'GALAPAGOS_HLS_VERSION' : answers['hls_ver'],
+           'GALAPAGOS_PART': answers['par_name'],
+           'GALAPAGOS_BOARD_NAME': answers['boa_name']
         }
 
 pprint(env_var)
 
 out_file = open("my_init.sh", "w") 
 
-out_file.write("source init.sh " + answers['galapagos_dir'] + ' ' + answers['vivado_dir'] + ' ' + answers['hls_dir'] + ' ' + answers['vivado_ver'] + ' ' + answers['hls_ver'])
+out_file.write("source init.sh " + answers['galapagos_dir'] + ' ' + answers['vivado_dir'] + ' ' + answers['hls_dir'] + ' ' + answers['vivado_ver'] + ' ' + answers['hls_ver'] + ' ' + answers['par_name'] + ' ' + answers['boa_name'] )
+
+
+
