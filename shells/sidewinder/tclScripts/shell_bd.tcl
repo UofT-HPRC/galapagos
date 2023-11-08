e26cf867885159dcac2060ab6c85be4acb34597c
camilo <juancamilovega555@gmail.com>
2023-11-08 10:47:10 -0500

    Add memory configuration for shell

--- shells/sidewinder/tclScripts/shell_bd.tcl
+++ shells/sidewinder/tclScripts/shell_bd.tcl
@@ -3,6 +3,6 @@ puts ${::env(GALAPAGOS_PATH)}
 source ${::env(GALAPAGOS_PATH)}/shells/sidewinder/tclScripts/shellTopLevel.tcl
 source ${::env(GALAPAGOS_PATH)}/shells/sidewinder/tclScripts/setup_ps_link.tcl
 source ${::env(GALAPAGOS_PATH)}/shells/sidewinder/tclScripts/ethernet.tcl
-#source ${::env(GALAPAGOS_PATH)}/shells/sidewinder/tclScripts/postprocessing.tcl
+source ${::env(GALAPAGOS_PATH)}/shells/sidewinder/tclScripts/postprocessing.tcl
 validate_bd_design
 return 0
